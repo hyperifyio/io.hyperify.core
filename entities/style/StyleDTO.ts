fc4c994bc3cbf9f98baab3d453aed54862587537
Jaakko Heusala <jhh@hg.fi>
2024-01-23 15:34:47 +0200

    Fixed types and added missing radius

--- entities/style/StyleDTO.ts
+++ entities/style/StyleDTO.ts
@@ -2,6 +2,7 @@
 
 import { BackgroundDTO } from "../background/BackgroundDTO";
 import { BorderDTO } from "../border/BorderDTO";
+import { BorderBoxDTO } from "../borderBox/BorderBoxDTO";
 import { ColorDTO } from "../color/ColorDTO";
 import { FontDTO } from "../font/FontDTO";
 import { SizeBoxDTO } from "../sizeBox/SizeBoxDTO";
@@ -18,7 +19,7 @@ export interface StyleDTO extends DTO {
     readonly height             ?: SizeDTO;
     readonly margin             ?: SizeDTO | SizeBoxDTO;
     readonly padding            ?: SizeDTO | SizeBoxDTO;
-    readonly border             ?: BorderDTO | [BorderDTO, BorderDTO] | [BorderDTO, BorderDTO, BorderDTO, BorderDTO];
+    readonly border             ?: BorderDTO | BorderBoxDTO;
     readonly font               ?: FontDTO;
     readonly textDecoration     ?: TextDecorationDTO;
     readonly background         ?: BackgroundDTO;
