48943868eb8b6690e883a957987c56be2a8b2026
Jaakko Heusala <jhh@hg.fi>
2024-09-10 14:51:12 +0300

    Improved unit tests for EntityUtils (https://github.com/hyperifyio/test/issues/12)

--- data/utils/EntityUtils.test.ts
+++ data/utils/EntityUtils.test.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2023. Heusala Group Oy <info@hg.fi>. All rights reserved.
+// Copyright (c) 2023-2024. Heusala Group Oy <info@hg.fi>. All rights reserved.
 
 import "../../../testing/jest/matchers/index";
 import { EntityUtils } from "./EntityUtils";
@@ -41,11 +41,27 @@ describe('EntityUtils', () => {
     // }
 
     describe('#getColumnName', () => {
-
         it('can get column name', () => {
             expect( EntityUtils.getColumnName('fooBar', [{fieldType: EntityFieldType.UNKNOWN, propertyName: 'fooBar', columnName: 'foo_bar', nullable: false, updatable: true, insertable: true}])).toBe('foo_bar');
         })
+    });
 
+    describe('#parseDateAsString', () => {
+        it('can parse undefined value as undefined', () => {
+            expect( EntityUtils.parseDateAsString(undefined) ).toBe(undefined);
+        })
+        it('can parse empty string as undefined', () => {
+            expect( EntityUtils.parseDateAsString("") ).toBe(undefined);
+        })
+        it('can parse a string', () => {
+            expect( EntityUtils.parseDateAsString('2023-04-23T10:51:32.000Z') ).toBe('2023-04-23T10:51:32Z');
+        })
+        it('can parse a Date object', () => {
+            expect( EntityUtils.parseDateAsString(new Date('2023-04-23T10:51:32.000Z')) ).toBe('2023-04-23T10:51:32Z');
+        })
+        it('can parse milliseconds', () => {
+            expect( EntityUtils.parseDateAsString(1682247092123) ).toBe('2023-04-23T10:51:32Z');
+        })
     });
 
 });
